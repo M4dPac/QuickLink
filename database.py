1a0f70458ddc3870a52a6e56d264f47b57ce950a
Anton Rybakov <pac8891@gmail.com>
2023-05-29 21:48:24 +0300

    add is_ method and added checks for one when receiving a response from the database

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
+import re
 import sqlite3
+
+from config import HOST
 from generate import generate_random_string
 
 
@@ -6,7 +9,6 @@ class DatabaseManager:
     def __init__(self) -> None:
         self.conn = sqlite3.connect('database.db')
         self.cursor = self.conn.cursor()
-        self.cursor.execute(''' DROP TABLE IF EXISTS links ''')
         self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS links (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,7 +18,7 @@ class DatabaseManager:
             ''')
 
     def add_short_url(self, long_url: str) -> str:
-        short_url = generate_random_string()
+        short_url = self.__generate_short_url()
         self.cursor.execute(f'''
         INSERT INTO links (short_url, long_url)
         VALUES ('{short_url}', '{long_url}')
@@ -28,29 +30,47 @@ class DatabaseManager:
         self.cursor.execute(f'''
         SELECT short_url FROM links WHERE long_url = '{long_url}'
         ''')
-        return self.cursor.fetchone()[0]
+        answer = self.cursor.fetchone()
+        if answer:
+            return answer[0]
 
     def get_long_url(self, short_url: str) -> str:
         self.cursor.execute(f'''
         SELECT long_url FROM links WHERE short_url = '{short_url}'
         ''')
-        return self.cursor.fetchone()[0]
+        answer = self.cursor.fetchone()
+        if answer:
+            return answer[0]
 
-    def delete_short_url(self, short_url: str) -> None:
+    def delete_url(self, url: str) -> None:
+        if self.__is_host_url(url):
+            self.cursor.execute(f'''
+            DELETE FROM links WHERE shorturl = '{url}'
+            ''')
+        else:
+            self.cursor.execute(f'''
+            DELETE FROM links WHERE longurl = '{url}'
+            ''')
+
+    def __generate_short_url(self) -> str:
+        while True:
+            short_url = generate_random_string()
+            if not self.__is_in_db(short_url):
+                return short_url
+
+    def __is_in_db(self, short_url: str) -> bool:
         self.cursor.execute(f'''
-        DELETE FROM links WHERE short_url = '{short_url}'
+        SELECT short_url FROM links WHERE short_url = '{short_url}'
         ''')
+        return bool(self.cursor.fetchone())
+
+    def drop_table(self):
+        self.cursor.execute(''' DROP TABLE IF EXISTS links ''')
+
+    @staticmethod
+    def __is_host_url(url: str) -> bool:
+        return bool(re.match(rf"https*://{HOST}/", url))
 
 
 if __name__ == "__main__":
-    db = DatabaseManager()
-    long_link = "https://www.google.com"
-    short_link = db.add_short_url(long_link)
-
-    current_link = db.get_short_url(long_link)
-    assert current_link == short_link, f"Wrong short link, curent link: {current_link}, expected link: {short_link}"
-
-    current_link = db.get_long_url(short_link)
-    assert current_link == long_link, f"Wrong long link, curent link: {current_link}, expected link: {long_link}"
-    db.delete_short_url(short_link)
-    print(db.cursor.execute(''' SELECT * FROM links''').fetchall())
+    pass
