3b2f8be5e2ab0b494def3e03fae182c5a7752965
Anton Rybakov <pac8891@gmail.com>
2023-06-03 17:26:32 +0300

    added the creating attribute to determine whether to create a database or not, added the drop_table method to reset the table, rewrite some sql queries

--- clients/sqlite3_client.py
+++ clients/sqlite3_client.py
@@ -6,8 +6,16 @@ from generate import generate_random_string
 
 
 class DatabaseManager:
-    def __init__(self, db_url: str = '../database.db') -> None:
+    def __init__(self,
+                 db_url: str = '../database.db',
+                 creating: bool = False) -> None:
+        '''
+        Инициализация класса
+        :param db_url: Адрес БД
+        :param creating: Отвечает за создание таблицы
+        '''
         self.db_url = db_url
+        self.creating = creating
 
     def __len__(self):
         command = ''' SELECT COUNT(*) FROM links; '''
@@ -18,14 +26,8 @@ class DatabaseManager:
     def __enter__(self) -> 'DatabaseManager':
         self.conn = sqlite3.connect(self.db_url)
         self.cursor = self.conn.cursor()
-        command = '''
-                CREATE TABLE IF NOT EXISTS links (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    short_url TEXT NOT NULL UNIQUE,
-                    long_url TEXT NOT NULL
-                    );
-                    '''
-        self.execute_command(command)
+        if self.creating:
+            self.create_table()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
@@ -39,8 +41,18 @@ class DatabaseManager:
         self.cursor.execute(command, params)
         self.conn.commit()
 
-    def add_short_url(self, long_url: str) -> str:
-        short_url = self.__generate_short_url()
+    def create_table(self):
+        command = '''
+                CREATE TABLE IF NOT EXISTS links (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    short_url TEXT NOT NULL UNIQUE,
+                    long_url TEXT NOT NULL
+                    );
+                    '''
+        self.execute_command(command)
+
+    def get_and_add_short_url(self, long_url: str) -> str:
+        short_url = self._generate_short_url()
         command = f'''
         INSERT INTO links (short_url, long_url) VALUES (?, ?);
          '''
@@ -71,27 +83,35 @@ class DatabaseManager:
         '''
         self.execute_command(command, (url,))
 
-    def __generate_short_url(self) -> str:
+    def drop_table(self):
+        commands = [
+            "DELETE FROM links;",
+            "DELETE FROM SQLITE_SEQUENCE WHERE name='links';",
+        ]
+        for command in commands:
+            self.execute_command(command)
+
+    def delete_table(self):
+        command = ''' DROP TABLE links; '''
+        self.execute_command(command)
+
+    def _generate_short_url(self) -> str:
         while True:
             short_url = generate_random_string()
-            if not self.__is_in_db(short_url):
+            if not self._is_in_db(short_url):
                 return short_url
 
-    def __is_in_db(self, short_url: str) -> bool:
+    def _is_in_db(self, short_url: str) -> bool:
         command = f'''
         SELECT short_url FROM links WHERE short_url = ?;
         '''
         self.execute_command(command, (short_url,))
         return bool(self.cursor.fetchone())
 
-    def drop_table(self):
-        command = ''' DROP TABLE IF EXISTS links; '''
-        self.execute_command(command)
-
 
 if __name__ == "__main__":
-    with DatabaseManager('../test_database.db') as client:
-        short_url = client.add_short_url('google.com/')
+    with DatabaseManager('../test_database.db', creating=True) as client:
+        short_url = client.get_and_add_short_url('google.com/')
         print(client.get_short_url('google.com/'))
         print(client.get_long_url(short_url))
         client.execute_command("SELECT * FROM links")
