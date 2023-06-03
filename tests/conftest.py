d5bf6950ade353ba86e67f51f6227a2d66ae541c
Anton Rybakov <pac8891@gmail.com>
2023-06-03 19:29:28 +0300

    finished the fixtures, rewrote the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 import sqlite3
 
-TEST_DB_URL = "../test_database.db"
+from config import TEST_DB_URL, TABLE_NAME
 
 
 @pytest.fixture(scope="session")
@@ -22,16 +22,16 @@ def db_cursor(db_connection):
 def create_moderate_tables(db_connection):
     """Фикстура для создания и удаления таблицы"""
 
-    create_user_table_query = '''
-        CREATE TABLE IF NOT EXISTS links (
+    create_user_table_query = f'''
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             short_url TEXT NOT NULL UNIQUE,
             long_url TEXT NOT NULL
             );
             '''
 
-    drop_user_table_query = """
-        DROP TABLE links;
+    drop_user_table_query = f"""
+        DROP TABLE {TABLE_NAME};
     """
 
     db_connection.execute(create_user_table_query)
@@ -39,3 +39,45 @@ def create_moderate_tables(db_connection):
     yield
     db_connection.execute(drop_user_table_query)
     db_connection.commit()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clean_table(db_connection):
+    """Фикстура для очистки бд"""
+    commands = f"""
+        DELETE FROM links;
+        DELETE FROM SQLITE_SEQUENCE WHERE name='{TABLE_NAME}';
+        """
+
+    db_connection.executescript(commands)
+    db_connection.commit()
+
+
+@pytest.fixture(scope="session")
+def create_test_url_in_database_function(db_connection):
+    """Фикстура для создания функции для наполнения бд"""
+    command = f"""
+        INSERT INTO {TABLE_NAME} (short_url, long_url) VALUES (?, ?);
+   """
+
+    def create_url(short_url, long_url):
+        """Функция для наполнения бд"""
+        db_connection.execute(command, (short_url, long_url))
+        db_connection.commit()
+
+    return create_url
+
+
+@pytest.fixture(scope="session")
+def create_read_url_from_database_function(db_cursor):
+    """Фикстура для создания функции для чтения из бд"""
+    command = f"""
+        SELECT * FROM {TABLE_NAME};
+   """
+
+    def read_url():
+        """Функция для чтения из бд"""
+        db_cursor.execute(command)
+        return db_cursor.fetchall()
+
+    return read_url
--- tests/test_sqlite3_client.py
+++ tests/test_sqlite3_client.py
@@ -1,58 +1,13 @@
 import pytest
-import sqlite3
 
 from clients.sqlite3_client import DatabaseManager
-
-TEST_DB_URL = "../test_database.db"
-DB_CONNECTION = sqlite3.connect(TEST_DB_URL)
-DB_CURSOR = DB_CONNECTION.cursor()
+from tests.conftest import TEST_DB_URL
 
 
-def create_url(short_url: str, long_url: str):
-    """ Функция для наполнения бд тестовыми данными"""
-    command = f'''
-            INSERT INTO links (short_url, long_url) VALUES (?, ?);
-            '''
-    DB_CURSOR.execute(command, (short_url, long_url))
-    DB_CONNECTION.commit()
-
-
-def read_table():
-    """ Функция для чтения из бд"""
-    command = f'''
-            SELECT * FROM links;
-            '''
-    DB_CURSOR.execute(command)
-    return DB_CURSOR.fetchall()
-
-
-def create_table():
-    """ Функция для создания таблицы"""
-    command = f'''
-            CREATE TABLE IF NOT EXISTS links (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            short_url TEXT NOT NULL,
-            long_url TEXT NOT NULL
-            );
-            '''
-    DB_CURSOR.execute(command)
-    DB_CONNECTION.commit()
-
-
-def drop_table():
-    """ Функция для удаления таблицы"""
-    command = f'''
-            DELETE FROM links;
-            '''
-    DB_CURSOR.execute(command)
-    DB_CONNECTION.commit()
-
-
-def test_write_to_db_by_command():
+def test_write_to_db_by_command(create_read_url_from_database_function):
     """
     Создаем независимое подключение к бд, выполняем команду для создания таблицы.
     С помощью клиента делаем запись в бд и проверяем результат.
-    :return: None
     """
 
     long_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
@@ -63,25 +18,24 @@ def test_write_to_db_by_command():
 
     # Проверяем результат
 
-    links = read_table()
+    links = create_read_url_from_database_function()
 
     assert len(links) == 1
     assert links[0][1] == short_url
     assert links[0][2] == long_url
 
 
-
-def test_read_from_db_client():
+def test_read_from_db_client(create_test_url_in_database_function,
+                             create_read_url_from_database_function):
     """
     Создаем независимое подключение к бд, выполняем команду для создания таблицы, записываем данные.
     С помощью клиента проверяем результат.
-    :return: None
     """
 
     short_url = 'CEBzuX'
     long_url = 'https://www.youtube.com/watch?v=dQw4w9WgXtQ'
 
-    create_url(short_url, long_url)
+    create_test_url_in_database_function(short_url, long_url)
 
     # Создаем клиент
     with DatabaseManager(TEST_DB_URL) as client:
@@ -91,7 +45,7 @@ def test_read_from_db_client():
         curent_length = len(client)
 
     # Проверяем результат
-    links = read_table()
+    links = create_read_url_from_database_function()
 
     assert len(links) == curent_length
     assert current_short_link == short_url
