bba64985296e44c2c751e174628c08834ff13fe9
Anton Rybakov <pac8891@gmail.com>
2023-06-11 23:48:29 +0300

    functions to work with

---
+++ any.py
@@ -0,0 +1,39 @@
+from clients.sqlite3_client import DatabaseManager
+import urllib.request, urllib.error
+
+from config import HOST
+
+
+def create_table():
+    with DatabaseManager(creating=True):
+        pass
+
+
+def drop_table():
+    with DatabaseManager() as client:
+        client.drop_table()
+
+
+def get_shorten_url(long_url: str) -> str:
+    with DatabaseManager() as client:
+        short_url = client.get_and_add_short_url(long_url)
+    return short_url
+
+
+def get_long_url(short_url: str) -> str:
+    with DatabaseManager() as client:
+        if not client.is_url_in_db(short_url):
+            return HOST
+        long_url = client.get_long_url(short_url)
+    return long_url
+
+
+def is_url_status(url: str) -> int:
+    try:
+        return urllib.request.urlopen(url).status == 200
+    except urllib.error.URLError:
+        return False
+
+
+if __name__ == "__main__":
+    drop_table()
